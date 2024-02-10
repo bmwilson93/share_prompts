c9ab5642eb20740da109c6adaacad980244b0dc1
Brian Wilson <bmwilson93@gmail.com>
2024-02-09 22:29:12 -0700

    created layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import '@styles/globals.css';
+
+export const metadata = {
+  title: "Promptorium",
+  description: 'Discover & Share AI Prompts'
+}
+
+const layout = () => {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default layout
