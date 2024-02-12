52036d271d742e91643b33fdf135754c75c04004
Brian Wilson <bmwilson93@gmail.com>
2024-02-12 01:41:40 -0700

    added session provider for next auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,8 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <Provider>
+
         <div className="main">
           <div className="gradient" />
         </div>
@@ -20,6 +22,8 @@ const RootLayout = ({ children }) => {
           <Nav />
           {children}
         </main>
+        
+        </Provider>
       </body>
     </html>
   )
