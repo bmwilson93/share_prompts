7a33007817cbd3f556894201f0a3298dee7b2725
Brian Wilson <bmwilson93@gmail.com>
2024-02-12 20:39:07 -0700

    updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: [['lh3.googleusercontent.com'],],
+//   }
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  }
+}
 
 export default nextConfig;
