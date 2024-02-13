13f9e4a1c3a5780a16a3f86e00e3f00d1278d16a
Brian Wilson <bmwilson93@gmail.com>
2024-02-12 20:39:41 -0700

    updated to display user image

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,18 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   }, [])
 
   return (
@@ -35,9 +35,10 @@ const Nav = () => {
         </p>
       </Link>
 
+
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt"
             className="black_btn">
@@ -52,7 +53,7 @@ const Nav = () => {
             </button>
 
             <Link href="/profile">
-              <Image src="/assets/images/logo.svg"
+              <Image src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -78,10 +79,10 @@ const Nav = () => {
       </div>
 
       {/* Mobile Navigation */}
-      <div className="sm:hidder flex relative">
-        {isUserLoggedIn ? (
+      <div className="sm:hidden flex relative">
+        {session?.user ? (
           <div className="flex">
-            <Image src="/assets/images/logo.svg"
+            <Image src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
